5bf3d43f94de73b434da70790077d09592b49477
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-23 12:22:02 +0000

    Add print_string.c

---
+++ print_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_string - prints a string to stdout.
+ * @args: list of arguments.
+ * @buffer: pointer to the buffer.
+ * @buf_i: buffer index.
+ *
+ * Return: 1 on success.
+ */
+int print_string(va_list args, char *buffer, unsigned int buf_i)
+{
+	char *str, none[] = "(null)";
+	unsigned int index = 0;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+	{
+		while (str[index])
+		{
+			buf_i = hand_buff(buffer, none[index], buf_i);
+			index++;
+		}
+		return (6);
+	}
+	while (str[index])
+	{
+		buf_i = hand_buff(buffer, str[index], buf_i);
+		index++;
+	}
+	return (index);
+}
