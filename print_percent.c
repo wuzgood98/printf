1bea168f61dabfa80efab3e50abc1f92751010ed
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-23 12:21:16 +0000

    Add print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_pctg - prints the character c.
+ * @args: list arguments.
+ * @b: pointer to the buffer.
+ * @b_i: buffer index.
+ *
+ * Return: 1 on success.
+ */
+int print_pctg(va_list args __attribute__((unused)), char *b, unsigned int b_i)
+{
+	hand_buff(b, '%', b_i);
+
+	return (1);
+}
