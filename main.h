dc1a72244a2368e3155491e35bac8c33fd455b73
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-23 12:15:44 +0000

    Add header file

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * struct dictionary - struct for printer functions
+ * @spec: specifier
+ * @print: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct dictionary
+{
+	char *spec;
+	int (*print)(va_list, char *, unsigned int);
+} dict_t;
+
+#define BUFF_SIZE 1024
+
+int _printf(const char *format, ...);
+int print_char(va_list args, char *buffer, unsigned int idx);
+int display_buff(char *buffer, unsigned int bytes);
+int (*spec_func(const char *str, int idx))(va_list, char *, unsigned int);
+int spec_func_count(const char *str, int idx);
+unsigned int hand_buff(char *buffer, char c, unsigned int buf_i);
+int print_string(va_list args, char *buffer, unsigned int buf_i);
+int print_pctg(va_list args __attribute__((unused)), char *b, unsigned int b_i);
+
+
+#endif
