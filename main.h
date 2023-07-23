715be5737e97018f4cd5fb83600ac4056a799220
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-23 18:09:28 +0000

    Add print_plus_int function prototype

--- main.h
+++ main.h
@@ -45,5 +45,6 @@ int print_address(va_list args, char *buffer, unsigned int buf_i);
 int print_hash_oct(va_list args, char *buffer, unsigned int buf_i);
 int print_hash_hexL(va_list args, char *buffer, unsigned int buf_i);
 int print_hash_hexU(va_list args, char *buffer, unsigned int buf_i);
+int print_plus_int(va_list args, char *buffer, unsigned int buf_i);
 
 #endif
