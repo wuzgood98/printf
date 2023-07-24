e514d52a880eb24a43ab9e460da8fa4f556a5ee4
crystalopaye <crystalopaye1@gmail.com>
2023-07-24 06:23:50 -0700

    update spec_func_count.c

--- spec_func_count.c
+++ spec_func_count.c
@@ -30,6 +30,10 @@ int spec_func_count(const char *str, int idx)
 		{" +d", print_plus_int}, {"+ i", print_plus_int},
 		{"+ d", print_plus_int}, {"R", print_rot13},
 		{"r", print_reverse}, {"ld", print_longint},
+		{"li", print_longint}, 
+
+
+
 		{NULL, NULL},
 	};
 	int index = 0, temp_idx, count = 0;
