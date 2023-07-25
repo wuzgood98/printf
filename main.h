a626eaa58854827ab0d353b5108a3f136543b7c0
crystalopaye <crystalopaye1@gmail.com>
2023-07-24 23:12:54 -0700

    update to main.h

--- main.h
+++ main.h
@@ -53,6 +53,6 @@ int print_longint(va_list args, char *buffer, unsigned int buf_i);
 int printluint(va_list args, char *buffer, unsigned int buf_i);
 char *fill_bin_array(char *bin, long int int_digit, int is_neg, int limit);
 char *fill_lng_oct_arr(char *bin, char *octal);
-
+int printlngoct(va_list args, char *buffer, unsigned int buf_i);
 
 #endif
