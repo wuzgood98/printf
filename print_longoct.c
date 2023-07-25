f231a4a8f81af52fe23c0b7302e23f96f8b6d67a
crystalopaye <crystalopaye1@gmail.com>
2023-07-24 23:01:38 -0700

    update to print_longoct.c

--- print_longoct.c
+++ print_longoct.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * printlngoct - prints the octal representation of long decimals.
+ * printlngoct - prints the octal representation
+ * of long decimals.
  * @args: list of arguments.
- * @buffer: pointer to the buffer.
+ * @buffer: pointer to the buffer where output
+ * will be stored.
  * @buf_i: buffer index.
  *
  * Return: the number of chars printed.
@@ -30,6 +32,23 @@ is_neg = 1;
 }
 
 binary = malloc(sizeof(char) * (64 + 1));
-binary =  
- 
- 
+binary = fill_bin_array(binary, input_int, is_neg, 64);
+octal = malloc(sizeof(char) * (22 + 1));
+octal = fill_lng_oct_arr(binary, octal);
+/* Print the octal representation and count
+ * the number of characters printed
+ */
+for (first_dig = a = count = 0; octal[a]; a++)
+{   
+if (octal[a] != '0' && first_dig == 0)
+first_dig = 1;
+if (first_dig)
+{
+buf_i = hand_buff(buffer, octal[a], buf_i);
+count++
+}
+}
+free(binary);
+free(octal);
+return (count);
+}
