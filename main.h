435b090d8f3632a70ea3296fcfc23d13f8179288
crystalopaye <crystalopaye1@gmail.com>
2023-07-25 03:55:01 -0700

    update main.h

--- main.h
+++ main.h
@@ -54,5 +54,8 @@ int printluint(va_list args, char *buffer, unsigned int buf_i);
 char *fill_bin_array(char *bin, long int int_digit, int is_neg, int limit);
 char *fill_lng_oct_arr(char *bin, char *octal);
 int printlngoct(va_list args, char *buffer, unsigned int buf_i);
+int printlnghex(va_list args, char *buffer, unsigned int buf_i);
+
+
 
 #endif
