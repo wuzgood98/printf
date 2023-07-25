e7de59e30ddbe20817ad7c0c2d350020fe1677ad
crystalopaye <crystalopaye1@gmail.com>
2023-07-25 08:53:18 -0700

    update header file

--- main.h
+++ main.h
@@ -55,5 +55,10 @@ int printlngoct(va_list args, char *buffer, unsigned int buf_i);
 int printlnghex(va_list args, char *buffer, unsigned int buf_i);
 int displaylupx(va_list args, char *buffer, unsigned int buf_i);
 int displayhint(va_list args, char *buffer, unsigned int buf_i);
+int displayhuint(va_list args, char *buffer, unsigned int buf_i);
+
+
+
+
 
 #endif
