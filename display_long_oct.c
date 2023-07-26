3f36fb89b7bd2b0ef8852fffaced49b04489d6e3
crystalopaye <crystalopaye1@gmail.com>
2023-07-25 22:26:21 -0700

    display long octal

--- display_long_oct.c
+++ display_long_oct.c
@@ -1,54 +1,68 @@
 #include "main.h"
 
 /**
- * printlngoct - prints the octal representation
- * of long decimals.
- * @args: list of arguments.
- * @buffer: pointer to the buffer where output
+ * displayloct - Prints long decimal number
+ * in octal.
+ * @args: Input argument list containing
+ * the long decimal number to print.
+ * @buffer: Buffer pointer where the output
  * will be stored.
- * @buf_i: buffer index.
+ * @buf_i: Index for the buffer pointer.
  *
- * Return: the number of chars printed.
+ * Return: Number of characters printed.
  */
 
-int printlngoct(va_list args, char *buffer, unsigned int buf_i)
+int displayloct(va_list args, char *buffer, unsigned int buf_i)
 {
-/* variable declarations */
-char *octal;
-char *binary;
-long int input_int, a, is_neg, count, first_dig;
+long int input_int;
+char *octal, *bin;
+unsigned int is_neg, a, cnt, is_first;
 
+/* Retrieve the long decimal number from
+ * the argument list
+ */
 input_int = va_arg(args, long int);
 is_neg = 0;
+
+/* Handle special case: if the input
+ * number is 0
+ */
 if (input_int == 0)
 {
 buf_i = hand_buff(buffer, '0', buf_i);
-return (1);  
+return (1);
 }
+
+/* Handle negative numbers by converting
+ * them to 2's complement
+ */
 if (input_int < 0)
 {
-input_int = (input_int * -1) -1;
+input_int = (input_int * -1) - 1;
 is_neg = 1;
 }
 
-binary = malloc(sizeof(char) * (64 + 1));
-binary = fill_bin_array(binary, input_int, is_neg, 64);
+/* Allocate memory for binary and octal arrays */
+bin = malloc(sizeof(char) * (64 + 1));
+bin = init_bin(bin, input_int, is_neg, 64);
 octal = malloc(sizeof(char) * (22 + 1));
-octal = fill_lng_oct_arr(binary, octal);
-/* Print the octal representation and count
- * the number of characters printed
- */
-for (first_dig = a = count = 0; octal[a]; a++)
-{   
-if (octal[a] != '0' && first_dig == 0)
-first_dig = 1;
-if (first_dig)
+
+/* Convert binary to octal representation */
+octal = init_oct(octal, bin);
+
+/* Print the octal number, omitting leading zeros */
+for (is_first = a = cnt = 0; octal[a]; a++)
+{
+if (octal[a] != '0' && is_first == 0)
+is_first = 1;
+if (is_first)
 {
 buf_i = hand_buff(buffer, octal[a], buf_i);
-count++
+cnt++;
+}		}
 }
-}
-free(binary);
+/* free allocated memory */
+free(bin);
 free(octal);
-return (count);
+return (cnt);
 }
